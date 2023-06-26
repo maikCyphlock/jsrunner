5eb5c282b0e1dc168d2345f14d0cb21c2f0d8d1c
maikCyphlock <maikolaguilar656@gmail.com>
2023-06-26 13:01:40 -0400

    entry point of the app

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import AppWrapper from './AppWrapper.tsx'
+
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  // <React.StrictMode>
+
+
+  <AppWrapper />
+
+  // </React.StrictMode>,
+)
+
+postMessage({ payload: 'removeLoading' }, '*')
