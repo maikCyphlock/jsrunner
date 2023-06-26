98b73923ba8ea3713444c90bc218cc95b1378f8b
maikCyphlock <maikolaguilar656@gmail.com>
2023-06-26 13:05:39 -0400

    babel parser to show the code elements

---
+++ src/lib/babel/stray-expression.ts
@@ -0,0 +1,94 @@
+import type { TraverseOptions, Node } from '@babel/traverse';
+
+
+export default function ({ types: t }: any): { visitor: TraverseOptions<Node> } {
+
+	function visit(path: any) {
+
+		if (path.parentPath.node.type !== 'VariableDeclarator') return;
+		if (path.parentPath.parentPath.parentPath.node.type !== 'Program') return;
+		const variableName = path.parentPath.node.id.name;
+
+		path.parentPath.parentPath.insertAfter(
+			t.callExpression(t.identifier('debug'), [
+				t.identifier(path.node.loc.start.line.toString()),
+				t.identifier(variableName)
+			])
+		);
+	}
+
+	function expression(path: any, replace?: null | undefined) {
+		if (replace == null) replace = path.node;
+
+		if (path.parentPath.node.type != 'ExpressionStatement') return;
+		if (path.node.callee?.identifier == 'debug') return;
+
+
+		if (path.parentPath.parentPath?.node?.type == 'WhileStatement') return;
+		if (path.parentPath.parentPath?.node?.type == 'ForStatement') return;
+
+		if (path.node.loc?.start == null) return;
+
+		path.replaceWith(
+			t.callExpression(t.identifier('debug'), [t.numericLiteral(path.node.loc.start.line), replace])
+		)
+
+	}
+
+	return {
+		visitor: {
+
+			BinaryExpression(path) {
+				expression(path);
+				visit(path);
+			},
+			UnaryExpression(path) {
+				expression(path);
+				visit(path);
+			},
+			CallExpression(path) {
+				if (path.node.callee['object'] && path.node.callee['object'].name == 'console') return;
+				expression(path);
+				visit(path);
+			},
+			AwaitExpression(path) {
+				expression(path);
+				visit(path);
+			},
+			NewExpression(path) {
+				expression(path);
+				visit(path);
+			},
+			DirectiveLiteral(path) {
+				if (!path.node?.value) return;
+				if (!path.node.loc?.start?.line) return;
+				path.parentPath.replaceWith(
+					t.callExpression(t.identifier('debug'), [
+						t.numericLiteral(path.node.loc.start.line),
+						t.stringLiteral(path.node.value)
+					])
+				);
+			},
+			Identifier(path) {
+				if (path.parentPath.type === 'VariableDeclarator') return
+
+				expression(path);
+				visit(path);
+			},
+			ArrayExpression(path) {
+				expression(path);
+				visit(path);
+			},
+			MemberExpression(path) {
+				expression(path);
+				visit(path);
+			},
+			Literal(path) {
+				if (path.parentPath.type === 'VariableDeclarator') return
+				if (path.type == 'TemplateLiteral') return;
+				expression(path);
+				visit(path);
+			}
+		}
+	};
+}
