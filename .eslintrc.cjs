058195b329cd48bc37fba6b5d8007c91fdbc35b8
maikCyphlock <maikolaguilar656@gmail.com>
2023-06-26 12:59:32 -0400

    initial commit- apha version

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',qq,
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+  },
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,jsx,tsx,js,ts}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
